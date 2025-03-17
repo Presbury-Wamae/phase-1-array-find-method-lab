721a8eb4f136184c4195580d362eca2c956e9b8b
Presbury <wamaepresbury034@gmail.com>
2025-03-17 23:36:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+// function superbowlWin(record){
+//     for(let item of record){
+//         if(item.result === "W"){
+//             return item.year
+//         }
+//         else{
+//             return "sadly, undefined"
+//         }
+//     }
+// }
+
+function superbowlWin(record){
+    let win = record.find(function(item){
+        return item.result === "W";
+    })
+        if (win){
+            return win.year;
+        }
+        else {
+            return undefined
+        }
+    } 
